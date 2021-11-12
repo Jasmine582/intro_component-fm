e7dd8a2f17329053e1ee4029a26c32779d1322b9
jasmine_r <joycesdaughter82@gmail.com>
2021-11-12 17:15:57 -0600

    Added alert for form

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 const claimButton = document.querySelector("button");
+
+function validateForm(){
+    let x = document.forms["fname"].value;
+    if(x==""){
+        alert("Name must be filled out");
+        return;
+    }
+}
